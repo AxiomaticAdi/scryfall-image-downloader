3e2a9c1ff032947a3bb9847bc5ecb722c7169e83
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-10-09 01:22:42 -0700

    Add instructions

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,16 @@ function App() {
 			}}
 		>
 			<h1>Magic Card Image Downloader for Billy</h1>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+				}}
+			>
+				<h3>Instructions for use:</h3>
+				Add one card per line in the format "[number] [card name]"
+				<i>Example:</i>1 sol ring
+			</div>
 			<div>
 				<CardEntryForm />
 			</div>
